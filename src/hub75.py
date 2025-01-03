59646b074c1bc2719c36ac90b3f0c22754e15b36
stef <stefdev49@gmail.com>
2025-01-03 22:16:09 +0100

    const bouge pas trop les temps

--- src/hub75.py
+++ src/hub75.py
@@ -1,6 +1,7 @@
 from machine import SoftSPI, freq, Pin
 from time import sleep_us
 import micropython
+from micropython import const
 
 #freq(160000000)  # default NodeMCU ESP-32S v1.1
 freq(240000000)
@@ -12,30 +13,30 @@ class Hub75SpiConfiguration:
     (https://hackerboxes.com/collections/past-hackerboxes/products/hackerbox-0065-realtime).
     '''
     def __init__(self):
-        self.spi_baud_rate = 2500000
+        self.spi_baud_rate = const(2500000)
 
         self.illumination_time_microseconds = 10
 
         # Row select pins
-        self.line_select_a_pin_number = 5
-        self.line_select_b_pin_number = 18
-        self.line_select_c_pin_number = 19
-        self.line_select_d_pin_number = 21
-        self.line_select_e_pin_number = 12
+        self.line_select_a_pin_number = const(5)
+        self.line_select_b_pin_number = const(18)
+        self.line_select_c_pin_number = const(19)
+        self.line_select_d_pin_number = const(21)
+        self.line_select_e_pin_number = const(12)
 
         # Hub75 RGB data pins
-        self.red1_pin_number = 2
-        self.blue1_pin_number = 15
-        self.green1_pin_number = 4
-        self.red2_pin_number = 16
-        self.blue2_pin_number = 27
-        self.green2_pin_number = 17
+        self.red1_pin_number = const(2)
+        self.blue1_pin_number = const(15)
+        self.green1_pin_number = const(4)
+        self.red2_pin_number = const(16)
+        self.blue2_pin_number = const(27)
+        self.green2_pin_number = const(17)
 
-        self.clock_pin_number = 22
-        self.latch_pin_number = 26
-        self.output_enable_pin_number = 25  # active low
+        self.clock_pin_number = const(22)
+        self.latch_pin_number = const(26)
+        self.output_enable_pin_number = const(25)  # active low
 
-        self.spi_miso_pin_number = 13  # not connected
+        self.spi_miso_pin_number = const(13)  # not connected
 
 
 class Hub75Spi:
