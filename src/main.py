fbf85ec808f13982a6b5547752152290c5a59573
Stef <stef.dev.49@gmail.com>
2025-01-04 08:57:37 +0100

    point de départ

--- src/main.py
+++ src/main.py
@@ -163,29 +163,28 @@ printat(0, 7, char_o, 7)
 printat(0, 14, char_n, 7)
 printat(0, 21, char_n, 7)
 printat(0, 28, char_e, 7)
-printat(0, 36, char_ex, 7)
 
-printat(12, 0, char_a, 7)
-printat(12, 7, char_n, 7)
-printat(12, 14, char_n, 7)
-printat(12, 21, char_e, 7)
-printat(12, 28, char_ea, 7)
+printat(0, 42, char_a, 7)
+printat(0, 49, char_n, 7)
+printat(0, 56, char_n, 7)
+printat(12, 0, char_e, 7)
+printat(12, 7, char_ea, 7)
 
+printat(12, 21, char_2, 7)
+printat(12, 28, char_0, 7)
 printat(12, 36, char_2, 7)
-printat(12, 43, char_0, 7)
-printat(12, 51, char_2, 7)
-printat(12, 58, char_5, 7)
+printat(12, 43, char_5, 7)
+printat(12, 50, char_ex, 7)
 
-# for col in range(0, 64):
-#     matrix.set_pixel_value(30, col, 7)
-#     matrix.set_pixel_value(31, col, col % 8)
+for col in range(0, 63):
+    matrix.set_pixel_value(31, col, 7)
 
 counter = 0
 start = time_ns()
 while True:
     hub75spi.display_data()
     counter += 1
-    if counter == 100:
+    if counter == 200:
         end = time_ns()
         print(f"durée = {(end - start)/(1000000*counter)} ms")
         counter = 0
